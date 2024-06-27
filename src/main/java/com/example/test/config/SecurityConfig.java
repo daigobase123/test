cdad3cdd3488797ecaf8b25ab10ddd849b2309b3
unknown <daigobase123@gmail.com>
2024-06-28 07:35:29 +0900

    r

--- src/main/java/com/example/test/config/SecurityConfig.java
+++ src/main/java/com/example/test/config/SecurityConfig.java
@@ -28,23 +28,8 @@ public class SecurityConfig {
 	    	.formLogin().usernameParameter("userName")          //ユーザ名のリクエストパラメータ名
 	        .passwordParameter("password")
 	        .successHandler(new CustomLoginSuccessHandler())
-	        .failureUrl("/ffff")
 	        ;         //パスワードのリクエストパラメータ名
-        // 認可の設定
-//        http.authorizeHttpRequests(authz -> authz
-//        		.requestMatchers("/").permitAll()           //ページは全ユーザからのアクセスを許可
-//                .requestMatchers("/login").permitAll()  //loginPageは、全ユーザからのアクセスを許可
-//                .anyRequest().authenticated()               //上記ページ以外は認証を求める
-//                );
-//        // ログイン設定
-//        http.formLogin(login -> login                   //フォーム認証の有効化
-//                .loginPage("/login")                //ログインフォームを表示するパス
-//                .loginProcessingUrl("/authenticate")    //フォーム認証処理のパス
-//                .usernameParameter("userName")          //ユーザ名のリクエストパラメータ名
-//                .passwordParameter("password")          //パスワードのリクエストパラメータ名
-//                .defaultSuccessUrl("/home")             //認証成功時に遷移するデフォルトのパス
-//                .failureUrl("/loginPage?error=true")    //認証失敗時に遷移するパス
-//        		);
+ 
 //        // ログアウト設定
 //        http.logout(logout -> logout
 //                .logoutSuccessUrl("/loginPage")         //ログアウト成功時に遷移するパス
--- src/main/java/com/example/test/handler/CustomLoginSuccessHandler.java
+++ src/main/java/com/example/test/handler/CustomLoginSuccessHandler.java
@@ -1,15 +1,20 @@
 package com.example.test.handler;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
+import com.example.test.ConnectionProvide;
 import com.example.test.beans.LoginUser;
-import com.example.test.dao.LoginDao;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
@@ -19,8 +24,13 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
 	
-	@Autowired
-    private LoginDao loginDao;
+//	@Autowired
+//    private LoginDao loginDao;
+	
+	 @Autowired
+	 private ConnectionProvide connectionProvider;
+	 
+	 private static final String INSER_QUERY = "INSERT INTO t_login (username, success) Values(?, ?)";
 	
     @Override
     public void onAuthenticationSuccess(final HttpServletRequest request, final HttpServletResponse response,
@@ -28,12 +38,24 @@ public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
     	
 //    	トランサクションの保存
 //    	LoginUser loginUser = this.loginDao.store(true);
+    	String empName = SecurityContextHolder.getContext().getAuthentication().getName();
+    	 try (Connection conn = connectionProvider.getConnection();
+    	            PreparedStatement stmt = conn.prepareStatement(INSER_QUERY, Statement.RETURN_GENERATED_KEYS); ) {
+    	            
+    	            stmt.setString(1, "test");
+    	            stmt.setBoolean(2, true);
+    	            stmt.executeUpdate();   	            
+
+    	       
+    	        } catch (SQLException e) {
+    	            throw new IllegalStateException(e);
+    	        }
     	LoginUser loginUser = new LoginUser();
-    	 loginUser.setRoleId(1);
-         loginUser.setEmpNo("10");
-         loginUser.setEmpName("ter");
+//    	 loginUser.setRoleId(1);
+//         loginUser.setEmpNo("10");
+//         loginUser.setEmpName("ter");
          
-         Cookie cookie = new Cookie("user", "1");
+         Cookie cookie = new Cookie("empName", empName);
          response.addCookie(cookie);
 //    
     	response.setStatus(HttpStatus.OK.value());
